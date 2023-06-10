f681c9a6933dfd12a91d6d843db95325c43da654
Hankscr459 <defcw58@gmail.com>
2023-06-10 13:55:40 +0800

    feat: cognito ResendConfirmCode

--- routes/aws/cognito.js
+++ routes/aws/cognito.js
@@ -2,6 +2,7 @@ const {
   CognitoIdentityProviderClient,
   SignUpCommand,
   ConfirmSignUpCommand,
+  ResendConfirmationCodeCommand,
 } = require('@aws-sdk/client-cognito-identity-provider');
 
 module.exports = async function (fastify, opts) {
@@ -43,11 +44,25 @@ module.exports = async function (fastify, opts) {
       region: 'ap-southeast-2',
     });
     const input = { // ConfirmSignUpRequest
-      ClientId: process.env.AWS_Cognito_ClientId, // required
+      ClientId: process.env.AWS_Cognito_ClientId,
       Username: email,
-      ConfirmationCode: code, // required
+      ConfirmationCode: code,
     };
     const command = new ConfirmSignUpCommand(input);
+    const response = await client.send(command);
+    return { success: true, data: response };
+  });
+
+  fastify.post('/resend-Confirm/code', async function (req, reply) {
+    const { email } = req.body;
+    const client = new CognitoIdentityProviderClient({
+      region: 'ap-southeast-2',
+    });
+    const input = { // ConfirmSignUpRequest
+      ClientId: process.env.AWS_Cognito_ClientId,
+      Username: email,
+    };
+    const command = new ResendConfirmationCodeCommand(input);
     const response = await client.send(command);
     return { success: true, data: response };
   });
